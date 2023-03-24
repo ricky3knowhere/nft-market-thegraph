cdb6a2c61b3eae726eb7ecb57833d7b3e2c4dfb6
ricky03knowhere <goose73thecat@gmail.com>
2023-03-24 09:33:01 +0700

    initial commit

--- src/nft-market.ts
+++ src/nft-market.ts
@@ -1,37 +1,19 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  NFTMarket,
-  Approval,
-  ApprovalForAll,
-  NFTTransfer,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/NFTMarket/NFTMarket"
-import { ExampleEntity } from "../generated/schema"
+import { NFTMarket, NFTTransfer } from "../generated/NFTMarket/NFTMarket";
+import { NFT } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
+export function handleNFTTransfer(event: NFTTransfer): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+  let nft = new NFT(event.params.tokenID.toString());
+  nft.to = event.params.to;
+  nft.from = event.params.from;
+  nft.price = event.params.price;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
+  const nftMatket = NFTMarket.bind(event.address);
+  const tokenURI = nftMatket.tokenURI(event.params.tokenID);
+  nft.tokenURI = tokenURI;
   // Entities can be written to the store with `.save()`
-  entity.save()
+  nft.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -59,10 +41,10 @@ export function handleApproval(event: Approval): void {
   // - contract.tokenURI(...)
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+// export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleNFTTransfer(event: NFTTransfer): void {}
+// export function handleNFTTransfer(event: NFTTransfer): void {}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+// export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleTransfer(event: Transfer): void {}
+// export function handleTransfer(event: Transfer): void {}
